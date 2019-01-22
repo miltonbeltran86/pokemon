f74e1bfe398f02a735829b9c62bfbbd6ee65ae65
cesar <uriel8acastro@gmail.com>
2019-01-22 16:44:21 -0500

    deatalle pokemon

---
+++ src/app/poke-main/containers/poke-card/poke-card.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { PokemonsService } from '../../services/pokemons.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-poke-card',
+  templateUrl: './poke-card.component.html',
+  styleUrls: ['./poke-card.component.css']
+})
+export class PokeCardComponent implements OnInit {
+
+ _pokeResult: {name: string, url: string};
+  _poke: any;
+
+  @Input() 
+  get poke() : {name: string, url: string}{
+    return this._pokeResult;
+  };
+
+  set poke(result : {name: string, url: string}){
+    this.pokeService.getPokemonByUrl(result.url)
+    .subscribe(
+      pokemon => {
+        this._poke = pokemon;
+      }
+    )
+  }
+  
+
+  id : string;
+  detailPokemon : any;
+  constructor(private pokeService: PokemonsService, private route:ActivatedRoute) { }
+
+  ngOnInit() {
+
+    this.route.params.subscribe((params: Params)=> { this.id = params.id
+
+    console.log("Entro a detail")
+
+    console.log(this.id)
+
+
+
+     this.pokeService.getPokemonByUrl( this.id).subscribe((data)=> {
+       
+      this.detailPokemon = data;
+      console.log(data)
+
+     
+    }); 
+    })   
+
+  }
+
+}
