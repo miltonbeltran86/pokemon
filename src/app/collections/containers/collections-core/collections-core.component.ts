53227f0fb24b01afbc453afdfd6ca28387d82bd8
mijabechi <miltonbeltran86@gmail.com>
2019-01-21 23:37:43 -0500

    Add creation collection

--- src/app/collections/containers/collections-core/collections-core.component.ts
+++ src/app/collections/containers/collections-core/collections-core.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonsService } from 'src/app/poke-main/services/pokemons.service';
 import { CollectionsService } from '../../services/collections.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
+//import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-collections-core',
@@ -9,17 +13,33 @@ import { CollectionsService } from '../../services/collections.service';
 })
 export class CollectionsCoreComponent implements OnInit {
 
-  constructor(private pokemonsService: PokemonsService,private collectionsServices: CollectionsService) { }
+  constructor(private pokemonsService: PokemonsService,private collectionsServices: CollectionsService, private authFire: AngularFireAuth) { }
 
-  collectionPoke: any[];
+  collectionPoke: Observable<any[]>;
   addCollection(col){
     this.pokemonsService.addCollection(col);
   }
 
+  removeCollection(col){
+    this.collectionsServices.removeCollection(col);
+  }
+
   ngOnInit() {
-    this.collectionsServices.listCollections().subscribe(list =>{
-      this.collectionPoke = list;
-    })
+    //this.collectionsServices.listCollections().subscribe(list =>{
+    //  this.collectionPoke = list;
+    //})
+
+    this.authFire.authState
+    .subscribe(
+      user => {          
+        this.collectionPoke =  this.collectionsServices.listCollections().snapshotChanges().pipe(
+          map(changes => 
+            changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+          ));
+          console.log("Revisar coleccion");
+          console.log(this.collectionPoke);
+      }
+    );   
   }
 
   displayCounter(data: any){
