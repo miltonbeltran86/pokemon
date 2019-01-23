86bba339b86677970d465f59e37aa7fb2ecf3f37
cesar <uriel8acastro@gmail.com>
2019-01-23 13:53:40 -0500

    cambios para pokemosnes similares

--- src/app/poke-main/containers/poke-card/poke-card.component.ts
+++ src/app/poke-main/containers/poke-card/poke-card.component.ts
@@ -9,39 +9,39 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class PokeCardComponent implements OnInit {
 
- _pokeResult: {name: string, url: string};
+  _pokeResult: { name: string, url: string };
   _poke: any;
 
-  @Input() 
-  get poke() : {name: string, url: string}{
+  @Input()
+  get poke(): { name: string, url: string } {
     return this._pokeResult;
   };
 
-  set poke(result : {name: string, url: string}){
+  set poke(result: { name: string, url: string }) {
     this.pokeService.getPokemonByUrl(result.url)
-    .subscribe(
-      pokemon => {
-        this._poke = pokemon;
-      }
-    )
+      .subscribe(
+        pokemon => {
+          this._poke = pokemon;
+        }
+      )
   }
-  
 
-  id : string;
-  detailPokemon : any;
-  constructor(private pokeService: PokemonsService, private route:ActivatedRoute) { }
+
+  id: string;
+  detailPokemon: any;
+  constructor(private pokeService: PokemonsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
 
-    this.route.params.subscribe((params: Params)=> { 
+    this.route.params.subscribe((params: Params) => {
       this.id = params.id
-    console.log("Entro a detail")
-    console.log(this.id)
-     this.pokeService.getPokemonByUrl( this.id).subscribe((data)=> {       
-      this.detailPokemon = data;
-      console.log(data)     
-    }); 
-    })   
+      console.log("Entro a detail")
+      console.log(this.id)
+      this.pokeService.getPokemonByUrl(this.id).subscribe((data) => {
+        this.detailPokemon = data;
+        console.log(data)
+      });
+    })
 
   }
 
